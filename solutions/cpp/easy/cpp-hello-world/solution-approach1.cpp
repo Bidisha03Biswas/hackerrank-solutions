32a922f20eb0b15b84265676af69f09e4c52fa32
Bidisha Biswas <bidisha03biswas@gmail.com>
2026-09-09 23:04:33 +0530

    ✅ Say "Hello, World!" With C++ [cpp] — approach1

---
+++ solutions/cpp/easy/cpp-hello-world/solution-approach1.cpp
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+// Problem     Say "Hello, World!" With C++
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-09, 11:04 p.m.
+// ──────────────────────────────────────────────────
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
