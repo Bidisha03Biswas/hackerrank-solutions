68c233c9157973633e916ffbfc4f507aa9b64dc9
Bidisha Biswas <bidisha03biswas@gmail.com>
2026-09-12 23:58:24 +0530

    ✅ Arrays Introduction [cpp] — approach1

---
+++ solutions/cpp/easy/arrays-introduction/solution-approach1.cpp
@@ -0,0 +1,33 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true
+// Problem     Arrays Introduction
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-09-12, 11:58 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }  
+    for(int i=n-1;i>=0;i--){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
